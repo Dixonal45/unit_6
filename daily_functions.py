98e7bf642e0427fee99cf3b4e05be6a8d11b7c4a
allison <allison.dixon@ssfs.org>
2019-10-28 10:33:28 -0400

    Initial commit

---
+++ daily_functions.py
@@ -0,0 +1,8 @@
+# created by Allison Dixon
+# last updated October 28, 2019
+# unit 6 daily functions
+
+# problem 1
+names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George",
+         "Harold", "Imogen", "Jackie", "Kurt", "Linda"]
+print(names[3:4])
