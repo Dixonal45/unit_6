46ea1057545ef83a3bbbc42f03d640b161f79c9c
allison <allison.dixon@ssfs.org>
2019-11-06 09:22:58 -0500

    finished unit 6 project

--- unit6_assignment1.py
+++ unit6_assignment1.py
@@ -1,11 +1,16 @@
 # by Allison Dixon
-# last updated November 5, 2019
+# last updated November 6, 2019
 # unit 6 assignment 1
 
 import random
 
 
 def are_duplicates(nums):
+    """
+    This function finds duplicate birthdays by comparing the items in a list
+    :param nums: the list
+    :return: True if there is a duplicate, False if there isn't
+    """
     for x in range(len(nums)):
         for y in range(x + 1, len(nums)):
             if nums[x] == nums[y]:
@@ -13,15 +18,18 @@ def are_duplicates(nums):
     return False
 
 
-num_runs = int(input("How many times do you want to run the simulation?"))
-duplicate_birthday = 0
-for x in range(num_runs):
-    birthdays = []
-    for x in range(23):
-        ran = random.randint(1, 365)
-        birthdays.append(ran)
-    print(are_duplicates(birthdays))
-    if are_duplicates(birthdays) is True:
-        duplicate_birthday += 1
-print((duplicate_birthday/num_runs) * 100)
-print("There were", duplicate_birthday, "sets duplicate birthdays.")
+def main():
+    num_runs = int(input("How many times do you want to run the simulation?"))
+    duplicate_birthday = 0
+    for x in range(num_runs):
+        birthdays = []
+        for y in range(23):
+            ran = random.randint(1, 365)
+            birthdays.append(ran)
+        if are_duplicates(birthdays) is True:
+            duplicate_birthday += 1
+    print("There were duplicate birthdays", (duplicate_birthday/num_runs) * 100, "% of the time.")
+    print(duplicate_birthday, "of the simulations run had duplicate birthdays.")
+
+
+main()
